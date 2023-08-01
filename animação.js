c1b73e81426a47d37a14e0309c818e726f120f27
DayRay <48932199+DayRay@users.noreply.github.com>
2023-08-01 19:58:33 -0300

    Atualização?

--- animação.js
+++
@@ -1,9 +0,0 @@
-const link = document.getElementById('link');
-
-link.addEventListener('click', () => {
-  link.classList.add('glitch-effect');
-
-  setTimeout(() => {
-    link.classList.remove('glitch-effect');
-  }, 2000);
-});
